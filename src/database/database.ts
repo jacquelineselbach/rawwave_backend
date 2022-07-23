5419b393dac8f8d315478e41b4eac6b598c2b47a
Markant <mark.perov@stud.fh-campuswien.ac.at>
2022-07-23 16:34:32 +0200

    add database

---
+++ src/database/database.ts
@@ -0,0 +1,43 @@
+import * as Mongoose from "mongoose";
+import { UserModel } from "./users/users.model";
+
+let database: Mongoose.Connection;
+
+export const connect = () => {
+  // add your own uri below
+  const uri =
+    "";
+
+  if (database) {
+    return;
+  }
+
+  Mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
+
+  database = Mongoose.connection;
+
+  database.once("open", async () => {
+    console.log("Connected to database");
+  });
+
+  database.on("error", () => {
+    console.log("Error connecting to database");
+  });
+
+  return {
+    UserModel,
+  };
+};
+
+export const disconnect = () => {
+  if (!database) {
+    return;
+  }
+
+  Mongoose.disconnect();
+};

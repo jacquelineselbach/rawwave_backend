0468b900bb4e8613753c85c85d6fd841a32138e0
Markant <mark.perov@stud.fh-campuswien.ac.at>
2022-07-23 16:36:53 +0200

    add DummyData

---
+++ src/scripts/createDummyData.ts
@@ -0,0 +1,22 @@
+import { UserModel } from "../database/users/users.model";
+import { connect, disconnect } from "../database/database";
+
+(async () => {
+  connect();
+
+  const users = [
+    { firstName: "Jacqueline", lastName: "Selbach", age: 27 },
+    { firstName: "Mark", lastName: "Perov", age: 31 }
+  ];
+
+  try {
+    for (const user of users) {
+      await UserModel.create(user);
+      console.log(`Created user ${user.firstName} ${user.lastName}`);
+    }
+
+    disconnect();
+  } catch (e) {
+    console.log(e);
+  }
+})();
